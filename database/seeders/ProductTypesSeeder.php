dfed5f0e8d3249e1b23155237b536d5a6cbc41c8
Adowalma <adowalmaadao@gmail.com>
2024-12-08 17:56:57 +0100

    #22 -Products seed updated

--- database/seeders/ProductTypesSeeder.php
+++ database/seeders/ProductTypesSeeder.php
@@ -15,9 +15,29 @@ class ProductTypesSeeder extends Seeder
     public function run()
     {
         //
-        DB::table('product_types')->insert([
+        DB::table('product_types')->insert(
+            [
             'tipo' => 'SAM GPS-CAM',
-            'image' => '20220418024921.jpeg',
+            'image' => '20220418024921.png',
+            'preco' => '20.000',
+            'quantidade' => 1,
+            'descricao' => nl2br("Dimensões 118,6 x 83,9 mm (D x P) \n
+            Peso
+            293 gr.\n
+            Gravação
+            por eventos, gravação manual, gravação contínua e reprodução remota\n
+            Resolução
+            HD 720p\n
+            Visão noturna
+            Sim"),
+            'created_at' => now(),
+            'updated_at' => now(),
+            ]
+        );
+            DB::table('product_types')->insert(
+            [
+            'tipo' => 'SAM Home-Kit',
+            'image' => '20220418015221.jpeg',
             'preco' => '52.000',
             'quantidade' => 1,
             'descricao' => nl2br("Dimensões 118,6 x 83,9 mm (D x P) \n
@@ -31,6 +51,61 @@ class ProductTypesSeeder extends Seeder
             Sim"),
             'created_at' => now(),
             'updated_at' => now(),
-        ]);
+            ]);
+            DB::table('product_types')->insert(
+            [
+            'tipo' => 'SAM Portable-GPS-CAM',
+            'image' => '20220418025628.jpeg',
+            'preco' => '48.000',
+            'quantidade' => 1,
+            'descricao' => nl2br("Dimensões 118,6 x 83,9 mm (D x P) \n
+            Peso
+            293 gr.\n
+            Gravação
+            por eventos, gravação manual, gravação contínua e reprodução remota\n
+            Resolução
+            HD 720p\n
+            Visão noturna
+            Sim"),
+            'created_at' => now(),
+            'updated_at' => now(),
+            ]);
+            DB::table('product_types')->insert(
+            [
+            'tipo' => 'SAM Kit-GPS-CAM',
+            'image' => '20220418013013.png',
+            'preco' => '52.000',
+            'quantidade' => 1,
+            'descricao' => nl2br("Dimensões 118,6 x 83,9 mm (D x P) \n
+            Peso
+            293 gr.\n
+            Gravação
+            por eventos, gravação manual, gravação contínua e reprodução remota\n
+            Resolução
+            HD 720p\n
+            Visão noturna
+            Sim"),
+            'created_at' => now(),
+            'updated_at' => now(),
+            ]);
+            DB::table('product_types')->insert(
+            [
+            'tipo' => 'SAM Mini-Portable-Kit',
+            'image' => '20220418133555.jpeg',
+            'preco' => '32.000',
+            'quantidade' => 1,
+            'descricao' => nl2br("Dimensões 118,6 x 83,9 mm (D x P) \n
+            Peso
+            293 gr.\n
+            Gravação
+            por eventos, gravação manual, gravação contínua e reprodução remota\n
+            Resolução
+            HD 720p\n
+            Visão noturna
+            Sim"),
+            'created_at' => now(),
+            'updated_at' => now(),
+            ],
+    );
     }
 }
